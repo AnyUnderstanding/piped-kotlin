60ed9cb70e3ca02837bada89bcf7e6e3acf03450
anyunderstanding <christopher.alan.w@gmail.com>
2024-04-10 12:38:13 +0200

    fix

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    antlr
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.antlr:antlr4:4.13.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+}
